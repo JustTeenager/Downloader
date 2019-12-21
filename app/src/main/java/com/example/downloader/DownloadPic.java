125b0cecf2b76493c93d32515d57e552f07d8101
JustTeenager <kotov.a.e@edu.mirea.ru>
2019-12-21 18:52:23 +0300

    Completed without wtf (mb losses on a as InputStream creating process

--- app/src/main/java/com/example/downloader/DownloadPic.java
+++ app/src/main/java/com/example/downloader/DownloadPic.java
@@ -8,12 +8,12 @@ import java.io.InputStream;
 import java.net.URL;
 
 public class DownloadPic extends AsyncTask {
-    InputStream a;
+    Bitmap bitmap;
     @Override
     protected Object doInBackground(Object[] objects) {
         try {
             URL url = new URL("https://daks2k3a4ib2z.cloudfront.net/56cf5dcdd3b4fc4579d08bef/56cf5dced3b4fc4579d08bf8_BomberMario-icon-300x300.jpg");
-            a=(InputStream) url.getContent();
+            bitmap= Bitmap.createBitmap(BitmapFactory.decodeStream((InputStream) url.getContent()));
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -22,7 +22,6 @@ public class DownloadPic extends AsyncTask {
 
     @Override
     protected void onPostExecute(Object o) {
-        Bitmap bitmap= Bitmap.createBitmap(BitmapFactory.decodeStream(a));
         MainActivity.doImg(bitmap);
     }
 }
